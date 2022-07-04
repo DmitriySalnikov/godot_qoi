48b4df41d0e4b42c573292e13e6890772c1bac26
Дмитрий Сальников <salnikov.mine@yandex.ru>
2022-07-04 03:13:00 +0300

    repetitive code in the build scripts has been moved to a separate action

--- build_godot_cpp_linux.sh
+++ build_godot_cpp_linux.sh
@@ -4,7 +4,7 @@ cpu=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
 #api=custom_api_file="../api.json"
 api= 
 
-git apply ../godot_cpp_restrict_used_classes.patch
+git apply ../godot_cpp_trim_unused_classes.patch
 
 scons platform=linux target=release bits=64 -j$cpu $api
 scons platform=linux target=debug bits=64 -j$cpu $api

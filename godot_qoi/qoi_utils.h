a74fb2da0d0dd5b6dbd4696851e3ed220efd9055
Дмитрий Сальников <salnikov.mine@yandex.ru>
2022-07-13 22:42:09 +0300

    all editor integration is moved to a separate folder changed the way to save resources as qoi file

--- godot_qoi/qoi_utils.h
+++ godot_qoi/qoi_utils.h
@@ -18,6 +18,7 @@ private:
 	PoolByteArray _get_qoi_cfg_magic() { return get_qoi_magic(); };
 
 	void _fake_set_prop_str(String str){};
+	String _get_import_error() { return QOI_IMPORT_ERROR; };
 	String _get_import_flags() { return QOI_IMPORT_FLAGS; };
 	String _get_import_size_limits() { return QOI_IMPORT_SIZE_LIMIT; };
 	String _get_import_fix_alpha() { return QOI_IMPORT_FIX_ALPHA_BORDER; };
@@ -28,6 +29,7 @@ private:
 public:
 	static const char qoi_cfg_magic[4];
 
+	static const char *QOI_IMPORT_ERROR;
 	static const char *QOI_IMPORT_FLAGS;
 	static const char *QOI_IMPORT_SIZE_LIMIT;
 	static const char *QOI_IMPORT_FIX_ALPHA_BORDER;
@@ -39,7 +41,9 @@ public:
 	void _init();
 
 	static PoolByteArray get_qoi_magic();
-	int add_footer(String target_path, Dictionary options);
+	int add_footer(String target_path, Dictionary options, bool update = true);
+	Dictionary read_footer(String target_path);
+	void update_dictionary(Dictionary &data, const Dictionary updates);
 
 	// If it were possible, the resource loading and saving classes would also be written in C++, but this is not possible...
 	// Therefore, only the main part of their logic is written in C++ and is used by GDScript scripts.

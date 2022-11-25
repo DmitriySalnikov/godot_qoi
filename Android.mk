b147acfa6ba60b42601f5eb1377bb73337daa4bb
Дмитрий Сальников <salnikov.mine@yandex.ru>
2022-11-25 15:09:55 +0300

    updated CI

--- Android.mk
+++ Android.mk
@@ -20,21 +20,21 @@ include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := godot_qoi
-LOCAL_CPPFLAGS := -std=c++14 -O3
-LOCAL_CPP_FEATURES := rtti exceptions
+LOCAL_CPPFLAGS := -std=c++17 -O3
+#LOCAL_CPP_FEATURES := rtti exceptions
 LOCAL_LDLIBS := -llog 
 #LOCAL_CFLAGS := -D_DEF
 
 LOCAL_SRC_FILES := \
-godot_qoi/qoi_utils.cpp \
+godot_qoi/qoi_import.cpp \
+godot_qoi/qoi_save.cpp \
 godot_qoi/qoi_wrapper.cpp \
 godot_qoi/register_types.cpp
 
 LOCAL_C_INCLUDES := \
 godot-cpp/godot-headers \
-godot-cpp/include/ \
-godot-cpp/include/core \
-godot-cpp/include/gen \
+godot-cpp/gen/include \
+godot-cpp/include/
 
 LOCAL_STATIC_LIBRARIES := godot-cpp
 

ff396c80769284bf5d080a0c370ca8833e8e24e0
Дмитрий Сальников <salnikov.mine@yandex.ru>
2022-11-30 00:27:01 +0300

    deleted .gdns from godot 3 decode_to_image is made private

--- godot_qoi/qoi_wrapper.h
+++ godot_qoi/qoi_wrapper.h
@@ -7,13 +7,17 @@ using namespace godot;
 class QOI : public RefCounted {
 	GDCLASS(QOI, RefCounted)
 
+	friend class QOIImport;
+
+private:
+	static int decode_to_image(const PackedByteArray &data, const Ref<Image> &out_image);
+
 protected:
 	static void _bind_methods();
 
 public:
 	static Ref<Image> read(String path);
 	static Ref<Image> decode(const PackedByteArray &data);
-	static int decode_to_image(const PackedByteArray &data, const Ref<Image> &out_image);
 	static int write(String path, Ref<Image> img);
 	static PackedByteArray encode(Ref<Image> img);
 };
